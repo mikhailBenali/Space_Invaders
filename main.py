eecd2ecc094c713a5d329282cd3de0b1c4095910
Mikhaïl <94171094+mikhailBenali@users.noreply.github.com>
2022-02-01 23:28:31 +0100

    Avancement tir incomplet
    Il reste encore à finir le tir de la balle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 # Initialisation
 pygame.init()
@@ -8,29 +9,51 @@ screen = pygame.display.set_mode((800, 600))  # Attention à bien rentrer un tup
 
 # Initialisation de l'icône et du titre
 pygame.display.set_caption("Space invaders")
-icone = pygame.image.load("fleche.jpg")
-pygame.display.set_icon(icone)
+icon = pygame.image.load("fleche.jpg")
+pygame.display.set_icon(icon)
+# Initialisation du fond d'écran
+background = pygame.image.load("background.jpg")
 
 # Joueur
-img_joueur = pygame.image.load("space-invaders.png")
-joueurX = 368
-joueurY = 500
-
-# Variables pour le déplacement
+player_img = pygame.image.load("space-invaders.png")
+player_x = 368
+player_y = 500
 x_change = 0
-y_change = 0
+
+# Ennemi
+enemy_img = pygame.image.load("enemy.png")
+enemy_x = randint(0, 800 - 64)
+enemy_y = randint(50, 150)
+enemy_x_change = 0.4
+
+# Balle
+bullet_img = pygame.image.load("bullet.png")
+bullet_x = 0
+bullet_y = 0
+bullet_y_change = 0.4
+bullet_state = "ready"  # "Ready" = Pas encore tiré / "Fired" = Sur l'écran
 
 
-def joueur(x, y):
-    screen.blit(img_joueur, (x, y))
+def player(x, y):
+    screen.blit(player_img, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemy_img, (x, y))
+
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bullet_img, (x + 16, y + 10))
 
 
 # Variable de conditionnement, pour arrêter le programme on passera cette variable à false
 running = True
 
 while running:
-    # Mise de l'arrière-plan en violet (RGB)
-    screen.fill((50, 50, 50))
+    # Apparition du fond d'écran
+    screen.blit(background, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -39,22 +62,47 @@ while running:
         # Déplacements
         # Lorsque les flèches sont pressées
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -0.1
-            if event.key == pygame.K_RIGHT:
-                x_change = 0.1
-            if event.key == pygame.K_DOWN:
-                y_change = 0.1
-            if event.key == pygame.K_UP:
-                y_change = -0.1
+            if event.key == pygame.K_LEFT or event.key == pygame.K_q:
+                x_change = -0.5
+            elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                x_change = 0.5
+            if event.key == pygame.K_SPACE and bullet_state == "ready":
+                fire_bullet(player_x, player_y)
+
+            # On quitte le programme e appuyant sur escape
+            elif event.key == pygame.K_ESCAPE:
+                running = False
 
         # Lorsque les flèches sont relâchées
         if event.type == pygame.KEYUP:
-            x_change = 0
-            y_change = 0
+            # Si les flèches gauche et droite sont relâchées
+            if event.key == pygame.K_LEFT or event.key == pygame.K_q or event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                # Le déplacement horizontal devient nul
+                x_change = 0
 
-    joueurX += x_change
-    joueurY += y_change
-    joueur(joueurX, joueurY)
+    # Modification de la position
+    player_x += x_change
+    if player_x <= 0:
+        player_x = 0
+    elif player_x >= 800 - 64:  # Taille horizontale de l'écran - Taille horizontale de l'image
+        player_x = 800 - 64
+
+    enemy_x += enemy_x_change
+
+    if enemy_x <= 0:
+        enemy_x_change = 0.4
+        enemy_x = 0
+        enemy_y += 64
+    elif enemy_x >= 800 - 64:
+        enemy_x = 800 - 64
+        enemy_x_change = -0.4
+        enemy_y += 40
+
+    if bullet_state == "fire":
+        bullet_y -= bullet_y_change
+        fire_bullet(player_x, player_y)
+
+    player(player_x, player_y)
+    enemy(enemy_x, enemy_y)
 
     pygame.display.update()
